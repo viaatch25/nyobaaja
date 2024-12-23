7db0302b1acaae2147034004904773e5f870c834
viaatch25 <bunnswiftiesbump@gmail.com>
2024-12-23 19:34:18 +0700

    Add files via upload

---
+++ halo indonesia/src/haloIndonesia.java
@@ -0,0 +1,18 @@
+
+
+ 
+
+
+ 
+ * @author via
+ */
+public class haloIndonesia {
+   
+    public static void main (String[] args){
+        System.out.println("Halo Indonesia");
+        System.out.println("Selamat Pagi Indonesia");
+        System.out.println("Jaya Indonesia");
+    }
+}
+
+
